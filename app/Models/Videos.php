1bbacf9165784c9ff92cb4bb8f84db7dc48c1cbf
Ilya Beliaev <ibeliaev@snud.de>
2025-03-01 21:51:27 +0100

    added video page, model and so on.

--- app/Models/Videos.php
+++ app/Models/Videos.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Videos extends Model
 {
-    //
+    protected $table = 'video';
+
+    public const STATUS_WAITING = 'WAITING';
+
+    public const STATUS_IN_PROGRESS = 'IN_PROGRESS';
+
+    public const STATUS_FINISHED = 'FINISHED';
 }
