76fe7b65d0b84a247199301bf3f016b1325a8ab6
Ilya Beliaev <ibeliaev@snud.de>
2025-03-01 21:52:39 +0100

    added video page, model and so on.

--- app/Models/Videos.php
+++ app/Models/Videos.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Videos extends Model
 {
@@ -13,4 +14,9 @@ class Videos extends Model
     public const STATUS_IN_PROGRESS = 'IN_PROGRESS';
 
     public const STATUS_FINISHED = 'FINISHED';
+
+    public function user(): BelongsTo
+    {
+        return new BelongsTo(User::class);
+    }
 }
