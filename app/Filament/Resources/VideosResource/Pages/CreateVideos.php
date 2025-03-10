4eaa8690ea49e230a82f65ab69c73762221b7df1
Ilya Beliaev <ibeliaev@snud.de>
2025-03-10 13:01:30 +0100

    added video page, model and so on.

--- app/Filament/Resources/VideosResource/Pages/CreateVideos.php
+++ app/Filament/Resources/VideosResource/Pages/CreateVideos.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\VideosResource\Pages;
 
 use App\Filament\Resources\VideosResource;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Notifications\Notification;
 
@@ -17,6 +19,13 @@ class CreateVideos extends CreateRecord
             ->success()
             ->title('User registered')
             ->body('The user has been created successfully.');
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            FileUpload::make('file_path')->required()->label('Video-File')
+        ]);
     }
 
 
