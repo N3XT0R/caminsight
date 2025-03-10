58aa262c7be277f5fa986615c46606e8bbc356c3
Ilya Beliaev <ibeliaev@snud.de>
2025-03-10 12:54:56 +0100

    added video page, model and so on.

--- app/Filament/Resources/VideosResource/Pages/CreateVideos.php
+++ app/Filament/Resources/VideosResource/Pages/CreateVideos.php
@@ -5,10 +5,19 @@ namespace App\Filament\Resources\VideosResource\Pages;
 use App\Filament\Resources\VideosResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateVideos extends CreateRecord
 {
     protected static string $resource = VideosResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('User registered')
+            ->body('The user has been created successfully.');
+    }
 
 
     protected function mutateFormDataBeforeCreate(array $data): array
