a64205d3995f93fd0b9f97463a1866f61858fc11
Ilya Beliaev <ibeliaev@snud.de>
2025-03-10 15:00:03 +0100

    added video page, model and so on.

--- resources/views/filament/pages/view-video.blade.php
+++ resources/views/filament/pages/view-video.blade.php
@@ -1,6 +1,6 @@
 <x-filament::page>
     <video controls>
-        <source src="{{ asset($this->record->file_path) }}" type="{{ $this->record->type }}">
+        <source src="{{ asset('videos/'.$this->record->file_path) }}" type="{{ $this->record->type }}">
         Your browser does not support the video tag.
     </video>
 </x-filament::page>
