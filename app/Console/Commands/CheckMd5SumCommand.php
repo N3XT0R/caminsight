80175f7a7b6cfbcd5b5a11bea3e04f70bb83d4b2
Ilya Beliaev <ibeliaev@snud.de>
2025-03-21 20:32:18 +0100

    some changes on getting video information from ID3.

--- app/Console/Commands/CheckMd5SumCommand.php
+++ app/Console/Commands/CheckMd5SumCommand.php
@@ -35,6 +35,8 @@ class CheckMd5SumCommand extends AbstractVideoBatchCommand
 
         $video->setAttribute('is_duplicated', $isDuplicated);
         if (true === $isDuplicated) {
+            $video->setAttribute('status', Videos::STATUS_FINISHED);
+            $video->save();
         }
         return $video->save() ? self::SUCCESS : self::FAILURE;
     }
